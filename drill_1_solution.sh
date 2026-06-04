728a10bccab3ee5c68a2ce9ddfeab0c34b098add
vinniiee <62161052+vinniiee@users.noreply.github.com>
2026-06-04 20:17:06 +0530

    Update echo statements to include task numbers and fix tree output

--- drill_1_solution.sh
+++ drill_1_solution.sh
@@ -3,11 +3,13 @@
 #1. Creating the required directory structure
 
 mkdir -p hello/one/two/three/four && mkdir -p hello/five/six/seven
-echo "Created the directories structure"
+echo "Task 1 : Created the directories structure"
 
 
-tree 2>error.log
-rm error.log
+#print update on shell
+cd hello
+tree 2>/dev/null
+cd ..
 sleep 2
 
 
@@ -22,19 +24,25 @@ touch hello/five/six/seven/error.log
 touch hello/one/two/three/four/access.log
 
 
-echo 'Created the required files'
+echo 'Task 2 : Created the required files'
 
-tree 2>error.log
-rm error.log
+#print update on shell
+
+cd hello
+tree 2>/dev/null
+cd ..
 sleep 2
 
 
 #3. Delete all the files having the .log extension
 find . -name "*.log" -exec rm {} \;
-echo 'Removed all the files having the .log extension'
+echo 'Task 3 : Removed all the files having the .log extension'
 
-tree 2>error.log
-rm error.log
+#print update on shell
+
+cd hello
+tree 2>/dev/null
+cd ..
 sleep 2
 
 #4. Add the following content to a.txt
@@ -42,38 +50,51 @@ sleep 2
 
 find . -name 'a.txt' -exec sh -c 'echo "Unix is a family of multitasking, multiuser computer operating systems that derive from the original AT&T Unix, development starting in the 1970s at the Bell Labs research center by Ken Thompson, Dennis Ritchie, and others" > "$1"' _ {} \;
 
-echo 'Added the required text to a.txt'
+echo 'Task 4 : Added the required text to a.txt'
 
 find . -name 'a.txt' -exec cat {} \;
 
-tree 2>error.log
-rm error.log
+#print update on shell
+
+cd hello
+tree 2>/dev/null
+cd ..
 sleep 2
 
 #5. Delete the directory named five
 
 find . -name 'five' -exec rm -r {} \;
-echo 'Deleted the directory named five'
-tree 2>error.log
-rm error.log
+echo 'Task 5 : Deleted the directory named five'
+
+#print update on shell
+
+cd hello
+tree 2>/dev/null
+cd ..
 sleep 2
 
 #6. Rename the one directory to uno
 
 find . -name 'one' -exec mv {} uno \;
 
-echo 'Renamed the one directory to uno'
-tree 2>error.log
-rm error.log
+echo 'Task 6 : Renamed the one directory to uno'
+
+#print update on shell
+
+cd hello
+tree 2>/dev/null
+cd ..
 sleep 2
 
 #7. Move a.txt to the two directory
-find . -name 'a.txt' -exec sh -c 'mv "$1" "$(find . -name "two")" ' _ {} \; 2>error.log
-rm error.log
+find . -name 'a.txt' -exec sh -c 'mv "$1" "$(find . -name "two")" ' _ {} \; 2>/dev/null
 
-echo 'Moved a.txt to the two directory'
+echo 'Task 7 : Moved a.txt to the two directory'
 
-tree 2>error.log
-rm error.log
+#print update on shell
+
+cd hello
+tree 2>/dev/null
+cd ..
 sleep 2
 
