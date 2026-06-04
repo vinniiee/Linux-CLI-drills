b94b5c7edb3fda3d69c027d2ceacf9f3a1b01453
vinniiee <62161052+vinniiee@users.noreply.github.com>
2026-06-04 20:55:08 +0530

    Refactor echo statements for better readability

--- drill_1_solution.sh
+++ drill_1_solution.sh
@@ -3,13 +3,13 @@
 #1. Creating the required directory structure
 
 mkdir -p hello/one/two/three/four && mkdir -p hello/five/six/seven
-echo "Task 1 : Created the directories structure"
+echo "\nTask 1 : Created the directories structure\n"
 
 
 #print update on shell
-cd hello
-tree 2>/dev/null
-cd ..
+tree hello 2>/dev/null
+
+echo "\n ####################################### \n"
 sleep 2
 
 
@@ -24,25 +24,24 @@ touch hello/five/six/seven/error.log
 touch hello/one/two/three/four/access.log
 
 
-echo 'Task 2 : Created the required files'
+echo '\nTask 2 : Created the required files\n'
 
 #print update on shell
 
-cd hello
-tree 2>/dev/null
-cd ..
+tree hello 2>/dev/null
+
+echo "\n ####################################### \n"
 sleep 2
 
 
 #3. Delete all the files having the .log extension
 find . -name "*.log" -exec rm {} \;
-echo 'Task 3 : Removed all the files having the .log extension'
+echo '\nTask 3 : Removed all the files having the .log extension\n'
 
 #print update on shell
 
-cd hello
-tree 2>/dev/null
-cd ..
+tree hello 2>/dev/null
+echo "\n ####################################### \n"
 sleep 2
 
 #4. Add the following content to a.txt
@@ -50,51 +49,49 @@ sleep 2
 
 find . -name 'a.txt' -exec sh -c 'echo "Unix is a family of multitasking, multiuser computer operating systems that derive from the original AT&T Unix, development starting in the 1970s at the Bell Labs research center by Ken Thompson, Dennis Ritchie, and others" > "$1"' _ {} \;
 
-echo 'Task 4 : Added the required text to a.txt'
+echo '\nTask 4 : Added the required text to a.txt\n'
 
 find . -name 'a.txt' -exec cat {} \;
 
 #print update on shell
 
-cd hello
-tree 2>/dev/null
-cd ..
+tree hello 2>/dev/null
+echo "\n ####################################### \n"
 sleep 2
 
 #5. Delete the directory named five
 
 rm -r hello/five
-echo 'Task 5 : Deleted the directory named five'
+echo '\nTask 5 : Deleted the directory named five\n'
 
 #print update on shell
 
-cd hello
-tree 2>/dev/null
-cd ..
+tree hello 2>/dev/null
+echo "\n ####################################### \n"
 sleep 2
 
 #6. Rename the one directory to uno
 
 mv hello/one hello/uno
 
-echo 'Task 6 : Renamed the one directory to uno'
+echo '\nTask 6 : Renamed the one directory to uno\n'
 
 #print update on shell
 
-cd hello
-tree 2>/dev/null
-cd ..
+
+tree hello 2>/dev/null
+echo "\n ####################################### \n"
 sleep 2
 
 #7. Move a.txt to the two directory
 find . -name 'a.txt' -exec sh -c 'mv "$1" "$(find . -name "two")"' _ {} \; 2>/dev/null
 
-echo 'Task 7 : Moved a.txt to the two directory'
+echo '\nTask 7 : Moved a.txt to the two directory\n'
 
 #print update on shell
 
-cd hello
-tree 2>/dev/null
-cd ..
+
+tree hello 2>/dev/null
+echo "\n ####################################### \n"
 sleep 2
 
