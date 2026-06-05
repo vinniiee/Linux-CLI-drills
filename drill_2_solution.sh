3a8f41216ffe849f88217b4447c824cc00c71233
vinniiee <62161052+vinniiee@users.noreply.github.com>
2026-06-05 18:33:14 +0530

    Complete remaining tasks of Processes, Ports, Managing Software and Miscellaneous sections

--- drill_2_solution.sh
+++ drill_2_solution.sh
@@ -45,3 +45,84 @@ echo "#######################################"
 echo 'Task 6 : Number of unique words present in the book : '
 tr -cs [:alpha:] '\n' <  hp.txt | tr [:upper:] [:lower:]  | sort -u | wc -l
 echo 
+
+
+echo "#######################################"   
+echo 'Task 7 : Browser PID & PPID'
+echo "#######################################"   
+
+ps -ax -o pid,ppid,comm | grep Safari
+
+echo "#######################################"   
+echo 'Task 8 : Kill browser'
+echo "#######################################"   
+pkill "Google Chrome"
+
+echo "#######################################"   
+echo 'Task 9 : Top CPU'
+echo "#######################################"   
+ps -Ao pid,%cpu,comm | sort -k2 -nr | head -4
+
+echo "#######################################"   
+echo 'Task 10 : Top Memory'
+echo "#######################################"   
+ps -Ao pid,%mem,comm | sort -k2 -nr | head -4
+
+echo "#######################################"   
+echo 'Task 11 : Start HTTP server on port 8000'
+echo "#######################################"   
+python3 -m http.server 8000
+
+echo "#######################################"   
+echo 'Task 12 : Stop HTTP server'
+echo "#######################################"   
+kill $(lsof -t -i:8000)
+
+echo "#######################################"   
+echo 'Task 13 : Start HTTP server on port 90'
+echo "#######################################"   
+sudo python3 -m http.server 90
+
+echo "#######################################"   
+echo 'Task 14 : All active connections'
+echo "#######################################"   
+lsof -i -P -n
+
+echo "#######################################"   
+echo 'Task 15 : PID on port 5432'
+echo "#######################################"   
+lsof -t -i:5432
+
+
+echo "#######################################"   
+echo 'Task 16 : Install vim, nginx and htop'
+echo "#######################################"
+brew install vim nginx htop
+
+echo "#######################################"   
+echo 'Task 17 : Uninstall nginx and remove its configuration files'
+echo "#######################################"
+brew uninstall nginx
+rm -rf /opt/homebrew/etc/nginx
+
+
+echo "#######################################"
+echo 'Task 18 : Get the IP address of the machine'
+echo "#######################################"
+ipconfig getifaddr en0
+
+echo "#######################################"
+echo 'Task 19 : Get the IP address of google.com'
+echo "#######################################"
+nslookup google.com
+
+echo "#######################################"
+echo 'Task 20 : check if Internet is working using CLI'
+echo "#######################################"
+ping 8.8.8.8
+
+echo "#######################################"
+echo 'Task 21 : Where is the node and code command located?'
+echo "#######################################"
+which node
+which code
